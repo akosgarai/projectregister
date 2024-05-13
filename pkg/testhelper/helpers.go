78066a7a76677e199549afe8d0d83f73183b98c2
akosgarai <szlem01@gmail.com>
2024-05-13 23:03:51 +0200

    Allow to return custom values in user repo mock

--- pkg/testhelper/helpers.go
+++ pkg/testhelper/helpers.go
@@ -5,35 +5,42 @@ import (
 )
 
 // UserRepositoryMock is a mock for the UserRepository interface.
+// It can be used to mock the UserRepository interface.
+// Set the LatestUser field to the user you want to return.
+// Set the AllUsers field to the list of users you want to return.
+// Set the Error field to the error you want to return.
 type UserRepositoryMock struct {
+	LatestUser *model.User
+	AllUsers   []*model.User
+	Error      error
 }
 
 // CreateUser mocks the CreateUser method.
 func (u *UserRepositoryMock) CreateUser(username, email, password string) (*model.User, error) {
-	return nil, nil
+	return u.LatestUser, u.Error
 }
 
 // GetUserByEmail mocks the GetUserByEmail method.
 func (u *UserRepositoryMock) GetUserByEmail(email string) (*model.User, error) {
-	return nil, nil
+	return u.LatestUser, u.Error
 }
 
 // GetUserByID mocks the GetUserByID method.
 func (u *UserRepositoryMock) GetUserByID(id int64) (*model.User, error) {
-	return nil, nil
+	return u.LatestUser, u.Error
 }
 
 // UpdateUser mocks the UpdateUser method.
 func (u *UserRepositoryMock) UpdateUser(user *model.User) error {
-	return nil
+	return u.Error
 }
 
 // DeleteUser mocks the DeleteUser method.
 func (u *UserRepositoryMock) DeleteUser(id int64) error {
-	return nil
+	return u.Error
 }
 
 // GetUsers mocks the GetUsers method.
 func (u *UserRepositoryMock) GetUsers() ([]*model.User, error) {
-	return nil, nil
+	return u.AllUsers, u.Error
 }
