6059d3695a1d011800776242163f5691997c7d3d
akosgarai <szlem01@gmail.com>
2024-06-25 22:23:01 +0200

    Add menu items

--- pkg/controller/dashboard.go
+++ pkg/controller/dashboard.go
@@ -2,15 +2,19 @@ package controller
 
 import (
 	"net/http"
+
+	"github.com/akosgarai/projectregister/pkg/model"
 )
 
 // DashboardController is the dashboard controller.
 func (c *Controller) DashboardController(w http.ResponseWriter, r *http.Request) {
 	template := c.renderer.BuildTemplate("dashboard", []string{c.renderer.GetTemplateDirectoryPath() + "/dashboard/index.html.tmpl"})
 	content := struct {
-		Title string
+		Title       string
+		CurrentUser *model.User
 	}{
-		Title: "Dashboard",
+		Title:       "Dashboard",
+		CurrentUser: c.CurrentUser(r),
 	}
 	err := template.ExecuteTemplate(w, "base.html", content)
 	if err != nil {
