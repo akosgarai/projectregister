d0c86b3d4e1780480fa5bcf4124f51690b633cf9
akosgarai <szlem01@gmail.com>
2024-07-29 13:56:29 +0200

    Resources type

--- pkg/model/resource.go
+++ pkg/model/resource.go
@@ -6,7 +6,21 @@ type Resource struct {
 	Name string
 }
 
+// Resources type is a slice of Resource
+type Resources []*Resource
+
+// ToMap converts the Resources to a map
+// The key is the string resource ID
+// The value is the resource Name
+func (r Resources) ToMap() map[int64]string {
+	result := make(map[int64]string)
+	for _, resource := range r {
+		result[resource.ID] = resource.Name
+	}
+	return result
+}
+
 // ResourceRepository interface
 type ResourceRepository interface {
-	GetResources() ([]*Resource, error)
+	GetResources() (*Resources, error)
 }
